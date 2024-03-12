228605e102488a42af48ad28efac0e19ad0da984
sina.ghorbanian <sinaleag79@gmail.com>
2024-03-12 14:53:13 +0330

    base structure (incomplete)

--- telegramBot.py
+++ telegramBot.py
@@ -1,13 +1,39 @@
 from dotenv import load_dotenv
 import os
-import telegram.ext
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    ContextTypes,
+)
 import asyncio
 
 
 load_dotenv("api_key.env")
 
 api_key = os.getenv("TELEGRAM_API_KEY")
+bot_username = "@sinaGpt_bot"
 
 
-updater = telegram.ext.Updater(api_key)
-dispatcher = 
+# Commands
+async def start_command(update: Update, Contex: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello, I am Sina's Humble Assistant")
+
+
+async def help_command(update: Update, Contex: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Please Contact My Master (Sina Ghorbanian) For any FFurther Assistant"
+    )
+
+
+# responses
+def handle_responses(text: str) -> str:
+    processed: str = text.lower()
+
+
+# message
+def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
